a5531977ec452f4b80ccfb6dc3aa44ebf304b011
Robin Scott <robinreyes31@hotmail.com>
2019-09-27 00:18:46 -0400

    Update game.js

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,17 @@
+var numberGuessed = [];
+var guessesLeft = 10;
+var wins = 0;
+
+var answer = 7;
+
+alert("Guess the number that the computer is thinking. You have 10 chances");
+
+for (i=0; i<10; i++) {
+    if (answer == numberGuessed);
+        alert=("CORRECT!"); {
+        
+    } else 
+    numberGuessed = prompt("try again");
+        
+    }
+}
