d0e30d748f5b7597a73b914985bdc1c32a8b51ff
Robin Scott <robinreyes31@hotmail.com>
2019-10-08 15:57:31 -0400

    "adding changes to html and js docs"

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,17 +1,18 @@
-var numberGuessed = [];
+var numberGuessed = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 var guessesLeft = 10;
 var wins = 0;
+var computerGuess = Math.floor(Math.random() * 11); 
 
 var answer = 7;
 
 alert("Guess the number that the computer is thinking. You have 10 chances");
 
-for (i=0; i<10; i++) {
+for (i = 0; i <11; i++) {
     if (answer == numberGuessed);
-        alert=("CORRECT!"); {
+        alert=("CORRECT!"); 
+        } 
         
-    } else 
+    { else 
     numberGuessed = prompt("try again");
         
     }
-}
