7996bcd057b24e311006baa72f65cfa2897be683
Robin Scott <robinreyes31@hotmail.com>
2019-10-08 17:21:55 -0400

    "added code to js"

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,18 +1,14 @@
-var numberGuessed = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var numberGuessed = [];
 var guessesLeft = 10;
 var wins = 0;
-var computerGuess = Math.floor(Math.random() * 11); 
+var computerGuess = Math.floor(Math.random() * 11) +1; 
+var userGuess = event.key;
 
-var answer = 7;
 
-alert("Guess the number that the computer is thinking. You have 10 chances");
 
-for (i = 0; i <11; i++) {
-    if (answer == numberGuessed);
-        alert=("CORRECT!"); 
-        } 
-        
-    { else 
-    numberGuessed = prompt("try again");
-        
-    }
+var computerGuess =
+    String.fromCharCode(
+        Math.floor(Math.random() * 11) +1
+    );
+
+console.log(computerGuess);
