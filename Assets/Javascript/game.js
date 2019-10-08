87ec6f9bbfc14000bacc2e71526bc2c60b61fab7
Robin Scott <robinreyes31@hotmail.com>
2019-10-08 17:40:22 -0400

    "added aler for win if player guess matches computer guess"

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -12,3 +12,68 @@ var computerGuess =
     );
 
 console.log(computerGuess);
+
+document.onkeydown = function(event) {
+    var keyPress = (String.fromCharCode(event.keyCode);
+    addNumber(keyPress);
+}
+
+
+function addNumber (usersKeypress) {
+
+     var repeatGuess = numberGuessed.some(function(item){
+        return item === usersKeypress;
+    })
+
+    if (repeatGuess) {
+        alert(usersKeypress + " already guessed. Try again!");
+
+    } else {
+        numberGuessed.push(usersKeypress);
+        console.log(numberGuessed);
+
+        shownumberGuessed();
+
+        guessMatch(usersKeypress);
+    }
+
+}
+
+function shownumberGuessed() {
+    var tempStr = numberGuessed.join(", ");
+    document.getElementById("playersGuess").innerHTML = tempStr;
+}
+
+function guessMatch (character) {
+
+    console.log(character);
+    console.log(computerGuess);
+
+    if (character === computerGuess) {
+
+        alert("You win!");
+        wins = wins + 1;
+        showWins();
+
+    } else if (guessesLeft === 0) {
+        alert("You lose. Play again.");
+        resetVariables ();
+
+    } else {
+        guessesLeft = guessesLeft - 1;
+        showGuessesRemaining();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
